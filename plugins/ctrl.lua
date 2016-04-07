1d1bde929ccb90df94aa765bed1f8b2b1f702a75
MasterKia <mysignup27@gmail.com>
2016-04-07 17:55:45 +0430

    Update ctrl.lua

--- plugins/ctrl.lua
+++ plugins/ctrl.lua
@@ -92,7 +92,7 @@ end
 	if matches[1] == 'rl' and is_sudo(msg) then
 		receiver = get_receiver(msg)
 		reload_plugins(true)
-		post_msg(receiver, "Reloaded!", ok_cb, false)
+		post_msg(receiver, ":]", ok_cb, false)
 		return ":]"
 	end
 end
